0a59dcbb02345a7231ed1eaa2d8b4a2173cb6bbe
Isckatel <nt-polza@yandex.ru>
2021-07-28 19:31:26 +0500

    Кнопка перезагрузки. Шрифт таймера.

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,8 @@ let oldTimeSeconds = 0;
 let timeBreak = 0;
 let audio = document.getElementsByTagName("audio")[0];
 var myWorker = new Worker("js/worker.js");
+myWorker.addEventListener('error', onError, false);
+countErr=0;
 let numTimer = 1;//номер таймера 1-работа 2-отдых
 let stop = false;
 let sentstop = false;
@@ -16,7 +18,9 @@ $("#button").on("click",function(){
   if (window.Worker) {
     myWorker.postMessage([timeSeconds]);
     numTimer = 1;
+    $('#type').html('Работа');
     $("#pause").css("display","inline-block");
+    $("#reload").css("display","inline-block");
     console.log('Строкой выше запустили воркер');
   } else {
     alert('Вашем браузере не поддерживается работа Веб-воркеров. Работа приложения не возможна.');
@@ -39,6 +43,11 @@ $("#continue").on("click",function(){
   stop = false;
 });
 
+//Нажата кнопка Перезагрузить
+$("#reload").on("click",function(){
+  window.location.reload();
+});
+
 myWorker.onmessage = function(e) {
   console.log('Ответ от воркера: ' + e.data[0]);
   if (e.data[0] == 'end') {
@@ -48,10 +57,12 @@ myWorker.onmessage = function(e) {
       console.log('Нужен новый воркер отдыха');
       myWorker.postMessage([timeBreak]);
       numTimer = 2;
+      $('#type').html('Отдых');
     } else if (numTimer==2) {
       console.log('Нужен новый воркер работы');
       myWorker.postMessage([timeSeconds]);
       numTimer = 1;
+      $('#type').html('Работа');
     }
   }
   else if(e.data[0] == 'stopon') {
@@ -66,3 +77,15 @@ myWorker.onmessage = function(e) {
 $("#delete").on("click",function(){
   myWorker.terminate();
 });
+
+function onError(e) {
+  console.log('Строка: ' + e.lineno);
+  console.log('В: ' + e.filename);
+  console.log('Сообщение: ' + e.message);
+  countErr +=1;
+  if (countErr>=3) {
+    myWorker.terminate();
+
+    alert('В потоке таймера произошла ошибка. Попробуйте перезагрузить страницу!');
+  }
+}
